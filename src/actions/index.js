d7ec7d58ebdd90087de93dff87715053ddbf35bb
Wallysson Nunes <wally.nm@gmail.com>
2016-09-14 09:33:50 -0300

    Component update

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a36707d2ecc2a40003dd4a9276f43fce';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	}
+}
